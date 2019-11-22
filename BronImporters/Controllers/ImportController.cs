986f38b5b6fcabf5d6baec27bfa2235642e6d92b
Niek de Gooijer <3060868+Xorpio@users.noreply.github.com>
2019-11-22 22:29:11 +0100

    added logic to send Employee to actor store

--- BronImporters/Controllers/ImportController.cs
+++ BronImporters/Controllers/ImportController.cs
@@ -1,4 +1,13 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Bronsysteem;
+using Bronsysteem.Actors;
+using Dapr.Actors.Client;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BronImporters.Controllers
@@ -8,8 +17,20 @@ namespace BronImporters.Controllers
     public class ImportController : ControllerBase
     {
         [HttpGet]
-        public HttpStatusCode Import()
+        public async Task<HttpStatusCode> Import()
         {
+            var jsonString = System.IO.File.ReadAllText("../GenerateData/Employees.json");
+            var employees = JsonSerializer.Deserialize<IList<Employee>>(jsonString);
+
+            var tasks = employees.Select(async emp =>
+            {
+                // some pre stuff
+                var actor = ActorProxy.Create<IEmployeeActor>(new Dapr.Actors.ActorId(emp.Guid.ToString()), "EmployeeActor");
+                var response = await actor.SetEmployeeAsync(emp);
+                Console.WriteLine(response);
+            });
+            await Task.WhenAll(tasks);
+
             return HttpStatusCode.OK;
         }
     }
--- BronImporters/Startup.cs
+++ BronImporters/Startup.cs
@@ -26,6 +26,7 @@ namespace BronImporters
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //services.AddTransient()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
