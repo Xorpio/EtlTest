236c56b83156db3e4706cc404a9b94f05a804609
Xorpio <3060868+Xorpio@users.noreply.github.com>
2019-11-24 20:58:52 +0100

    added IAMEmployee

---
+++ Bronsysteem/IAM/IAMEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bronsysteem.IAM
+{
+    public class IAMEmployee
+    {
+        public Guid Guid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? MiddelName { get; set; }
+        public IAMEmployee(Guid? guid, string firstName, string lastName, string? middelName)
+        {
+            Guid = guid ?? Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            MiddelName = middelName;
+        }
+
+        public IAMEmployee() {
+            FirstName = null!;
+            LastName = null!;
+            MiddelName = null!;
+        }
+    }
+}
